3ab78de0e2578f27656dcf68432b09a203d7c4a9
Anyamah Ndidi <anyamahedwin@gmail.com>
2023-11-09 06:34:14 +0100

    swc fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+
 import Hero from "@/components/Hero";
 import MovieCard from "@/components/commons/MovieCard";
 import SearchInput from "@/components/SearchInput";
